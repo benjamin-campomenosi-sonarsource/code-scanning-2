73eaab1c34ec695c48fcb0efaa55853484700ff9
Benjamin Campomenosi <benjamin.campomenosi@sonarsource.com>
2022-09-05 11:01:39 +0200

    first commit

---
+++ src/main/java/com/codescanning/demo/Starter.java
@@ -0,0 +1,11 @@
+package com.codescanning.demo;
+
+import java.io.IOException;
+
+public class Starter {
+
+  public static void main(String[] args) throws IOException {
+    VulnerabilityGenerator generator = new VulnerabilityGenerator();
+    generator.myVunerability1();
+  }
+}
