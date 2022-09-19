80c6f14d2031cac53cbcb5ab9e82e6b6dcbdc042
Benjamin Campomenosi <benjamin.campomenosi@sonarsource.com>
2022-09-19 09:31:35 +0200

    create 4 vulnerabilities for validation

--- src/main/java/com/codescanning/demo/Starter.java
+++ src/main/java/com/codescanning/demo/Starter.java
@@ -7,5 +7,12 @@ public class Starter {
   public static void main(String[] args) throws IOException {
     VulnerabilityGenerator generator = new VulnerabilityGenerator();
     generator.myVunerability1();
+    generator.openToWontFix();
+    generator.openToUsedInTest();
+    generator.openToFalsePositive();
+    generator.closeToOpen();
   }
+
+
+
 }
